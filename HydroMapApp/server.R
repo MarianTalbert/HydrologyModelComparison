2403982d966ef03f8bbbc7844eb2cb3905e2b6f9
Talbert <mtalbert@usgs.gov>
2016-05-11 11:36:32 -0600

    getting ready to put the data in a shiny app

--- HydroMapApp/server.R
+++ HydroMapApp/server.R
@@ -6,66 +6,50 @@ shinyServer(function(input, output,session) {
   
   #======================================	
   # create the map
-  values <- reactiveValues(rangeIndx=1)
+ 
+  
   Mapi=reactive({   
     switch(input$mapVar,
-           Temperature = 1,
-           Precipitation = 2,
-           Elevation =3)})
+           swe = 1,
+           runoff = 2,
+           smc =3,
+           et=4
+           )})
   
   MapLab=reactive({
     switch(input$mapVar,
-           Temperature = "Average Annual Temperature (F)",
-           Precipitation = "Total Annual Precipitation (In/Yr)",
-           Elevation ="Elevation")})
+           swe = "Snow Water Equivilant",
+           runoff = "Runoff",
+           smc ="Soil Moisture Content",
+           et ="Evapotranspiration Actual")})
   
   MapLst<-reactive({
-    if(input$mapVar=="Temperature") return(ShinyMapLst[[1]])
-    if(input$mapVar=="Precipitation") return(ShinyMapLst[[2]])
-    if(input$mapVar=="Elevation") return(ShinyMapLst[[3]])
+    if(input$mapVar=="swe") return(ShinyMapLst[[1]])
+    if(input$mapVar=="runoff") return(ShinyMapLst[[2]])
+    if(input$mapVar=="smc") return(ShinyMapLst[[3]])
   })
-  MapPal=reactive({
+  MapPal1=reactive({
     
-    values$rangeIndx<-switch(input$mapVar,
-                             Temperature = 1,
-                             Precipitation = 2,
-                             Elevation =3)
+      pal = colorNumeric("OrRd",domain=c(19,100),
+                         na.color = "transparent")
+   
+    return(pal)
+  })
+  MapPal2=reactive({
     
-    if(input$mapVar=="Temperature"& !input$diffFromHist)
-      pal = colorNumeric("OrRd", VarRng[[values$rangeIndx]],
-                         na.color = "transparent")
-    if(input$mapVar=="Temperature"& input$diffFromHist){
-      values$rangeIndx<-5
-      #browser()
-      pal = colorNumeric("YlOrRd", VarRng[[values$rangeIndx]],
-                         na.color = "transparent")
-    }
-    if(input$mapVar=="Precipitation" & !input$diffFromHist)
-      pal = colorNumeric("BuGn", VarRng[[values$rangeIndx]],
-                         na.color = "transparent")
-    if(input$mapVar=="Precipitation" & input$diffFromHist){
-      values$rangeIndx<-4
-      pal = colorNumeric("BrBG", VarRng[[values$rangeIndx]],
-                         na.color = "transparent")
-    }
-    if(input$mapVar=="Elevation") 
-      pal = colorNumeric(terrain.colors(10), VarRng[[values$rangeIndx]],
-                         na.color = "transparent")
+    pal = colorNumeric("Blues",domain=c(19,100),
+                       na.color = "transparent")
+    
     return(pal)
   })
   output$Map <- renderLeaflet({
     #This is getting to be a mess I should probably write out the rasters
     #to a the file system and construct their names from the input choices
-    TimePeriod<-switch(input$mapTime,
-                       "1990s"=1,
-                       "2040s"=2,
-                       "2080s"=3)
-    if(input$mapVar=="Elevation") TimePeriod<-1
+   
+    TimePeriod<-as.numeric(input$mapTime)
+   
     RcpChoice<-1
-    if(TimePeriod!=1){
-      RcpChoice<-switch(input$mapRCP,
-                        "High (RCP 8.5)"=2,
-                        "Mid Low (RCP 4.5)"=1)}
+  
     diffMap<-input$diffFromHist
     dataset <- MapLst()
     Title<-MapLab()
@@ -78,26 +62,16 @@ shinyServer(function(input, output,session) {
       diffMap=FALSE
     }
     
-    if(TimePeriod!=1 & diffMap){
-      if(Title=="Total Annual Precipitation"){
-        dataset[[TimePeriod]][[RcpChoice]]<-
-          100*(dataset[[TimePeriod]][[RcpChoice]]-dataset[[1]][[1]])/dataset[[1]][[1]]
-        Title<-"Percent Change in Precipitation"
-        
-      }
-      else{ 
-        
+ 
         dataset[[TimePeriod]][[RcpChoice]]<-
           (dataset[[TimePeriod]][[RcpChoice]]-dataset[[1]][[1]])
         Title<-"Change in Temperature"
-        
-      }
-    }
     
-    
-    MyMap<-leaflet() %>% addTiles() %>%  addRasterImage(dataset[[TimePeriod]][[RcpChoice]], colors = MapPal(), 
-                                                        opacity = input$mapTrans) %>%
-      addLegend(pal = MapPal(), values = VarRng[[values$rangeIndx]],title=Title)
+    MyMap<-leaflet() %>% addTiles() %>%  addRasterImage(dataset[[TimePeriod]][[RcpChoice]],
+                  colors = MapPal1(), 
+                  opacity = input$mapTrans) %>%
+      addLegend(pal = MapPal1(), values = VarRng[[1]],title=Title) %>%
+      addLegend(pal = MapPal2(), values = VarRng[[1]],title=Title)
    
     return(MyMap)
   })
--- HydroMapApp/ui.R
+++ HydroMapApp/ui.R
@@ -2,20 +2,14 @@
 ui = fluidPage(fluidRow(
     column(2,
            selectInput("mapVar", "Variable",
-                       choices=c("Snow Water Equivilant","Runoff","Soil Moisture Content","ET"),
-                       selected="Snow Water Equivilant")),
-    column(2,
-           selectInput("mapRCP", "Emissions Path",
-                       choices=c("High (RCP 8.5)","Mid Low (RCP 4.5)"),
-                       selected="Mid Low (RCP 4.5)")),
+                       choices=c("swe","runoff","smc","et"),
+                       selected="swe")),
     column(2,
            selectInput("mapTime", "Time Period",
-                       choices=c("Annual","Jan","Feb","March","April","May","June","July",
-                                 "August","September","October","November","December"),
-                       selected="1990s")),
-    column(2,sliderInput("mapTrans","Transparency", 0, 1,.8)),    
-    column(2,
-           checkboxInput("diffFromHist", label = "Show difference from historic period", value = FALSE))
+                       choices=list(Annual=13,January=1,
+                                    February=2,March=3,April=4),
+                       selected=13)),
+    column(2,sliderInput("mapTrans","Transparency", 0, 1,.8)) 
   ),
   leafletOutput('Map')
 )
